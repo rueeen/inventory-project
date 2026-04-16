1b97a8800580f90f745c9efb0b5df3aff04bd0d3
Ruben Valencia Arancibia <rvalencia@inacap.cl>
2026-04-15 22:01:42 -0400

    fix: resolve urls errors

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,5 +1,7 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
+from .forms import LoginForm
 from .views import (
     EquipmentCreateView,
     EquipmentDeleteView,
